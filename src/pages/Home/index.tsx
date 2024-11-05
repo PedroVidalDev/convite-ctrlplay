ed373f1f0085bbdfb84cdcaf4104408ac746d485
PedroVidalDev <pedrohvidals@gmail.com>
2024-11-05 17:42:23 -0300

    feat: add toast notifications

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,8 @@ import { Controller, useForm } from "react-hook-form"
 import { Container, InputText, Submit, Title, Video } from "./styles"
 import { NameForm } from "../../types/nameForm"
 import axios from "axios"
+import { ToastContainer, toast } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Home = () => {
 
@@ -12,11 +14,40 @@ export const Home = () => {
     })
 
     const onSubmit = async (data: NameForm) => {
-        await axios.post("http://localhost:3000/send-email", data);
+        try{
+            await axios.post("http://localhost:3000/send-email", data);
+            toast("Presença confirmada com sucesso!", {
+                onClose: () => {
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 500);
+                }
+            });
+        } catch (error) {
+            toast("Erro ao confirmar presença.", {
+                onClose: () => {
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 500);
+                }
+            });        
+        }
     }
 
     return (
         <Container>
+            <ToastContainer 
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
             <Title> Veja o vídeo! </Title>
             <Video />
             <Controller
