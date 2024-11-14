eb3214c84f9bae089f71c4905792191915ecdd5c
PedroVidalDev <pedrohvidals@gmail.com>
2024-11-14 15:36:27 -0300

    yeyyy

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const form = document.querySelector("#form");
 const body = document.querySelector("body");
+const button = document.querySelector("#submit");
 
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
+
+    button.disabled = true;
+    button.style.backgroundColor = "#ffb889";
 
     const formData = new FormData(event.target);
     const object = Object.fromEntries(formData);
