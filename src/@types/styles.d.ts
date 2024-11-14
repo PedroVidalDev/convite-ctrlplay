0e4895b513164012e74d1407623049f78b5225ed
PedroVidalDev <pedrohvidals@gmail.com>
2024-11-14 12:51:36 -0300

    reset

--- src/@types/styles.d.ts
+++
@@ -1,9 +0,0 @@
-import "styled-components"
-
-import { theme } from "../theme/theme"
-
-type ThemeType = typeof theme
-
-declare module 'styled-components' {
-    export interface DefaultTheme extends ThemeType{}
-}
--- src/types/nameForm.ts
+++
@@ -1,3 +0,0 @@
-export type NameForm = {
-    name: string
-}
